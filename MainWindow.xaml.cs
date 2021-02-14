865af433066c70ac6252cc4fdc94d3c4f869f8ad
Andrew Evsugin <ondrya@yandex.ru>
2021-02-15 00:06:58 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HandleKeyDown
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private List<string> RequiredFields = new List<string>()
+        {
+            "Required01", "Required02", "Required03"
+        };
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl))
+            {
+                foreach (var item in RequiredFields)
+                {
+                    var label = (Label)this.FindName(item);
+                    label.Background = label.Background == Brushes.OrangeRed ? Brushes.LightGray : Brushes.OrangeRed;
+                }
+            }
+        }
+    }
+}
